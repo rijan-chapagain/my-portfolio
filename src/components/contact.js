9b5f01ac1af898835ff0baaa9755297b026aeef8
Rijan Chapagain <33564044@student.murdoch.edu.au>
2021-08-08 17:11:56 +0800

    updates assets

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,43 +1,43 @@
 import React, { Component } from 'react';
 import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
-import myPic from '../img/me.jpg'
+// import myPic from 'img/me.jpg';
 
 class Contact extends Component {
   render() {
-    return(
+    return (
       <div className="contact-body">
         <Grid className="contact-grid">
           <Cell col={6}>
             <h2>Rijan Chapagain</h2>
             <img
-              src= { myPic }
+              src='url(/img/me.jpg)'
               alt="My_photo"
-              style={{height: '250px'}}
-               />
-             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>
-              Jnr. Mobile and Web app developer using NodeJS, React, jQuery. 
-              Common libraries of use: fs, node-formidable, Axios, Redux, body-parser. 
+              style={{ height: '250px' }}
+            />
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
+              Jnr. Mobile and Web app developer using NodeJS, React, jQuery.
+              Common libraries of use: fs, node-formidable, Axios, Redux, body-parser.
               Common use of UI libraries: Bootstrap 4, React-Bootstrap, CSS, canvasJs, jQuery table.
-             </p>
+            </p>
 
           </Cell>
           <Cell col={6}>
             <h2>Contact Me</h2>
-            <hr/>
+            <hr />
 
             <div className="contact-list">
               <List>
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-phone-square" aria-hidden="true" />
                     0452 659 223
                   </ListItemContent>
                 </ListItem>
 
                 <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-envelope" aria-hidden="true"/>
-                      hrijon.chp@gmail.com
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton' }}>
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    hrijon.chp@gmail.com
                   </ListItemContent>
                 </ListItem>
 
